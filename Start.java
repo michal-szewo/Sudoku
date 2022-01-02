411362a863cc362935206b5fab2a0612b4bf40c3
Michał <abc@xyz.pl>
2022-01-02 17:52:29 +0100

    first version

--- Start.java
+++ Start.java
@@ -2,10 +2,14 @@ import javax.swing.*;
 
 
 public class Start {
-
+	public static final int GRID_ROWS = 3;
+	public static final int GRID_COLUMNS = 3;
+	public static final int BOARD_ROWS = 9;
+	public static final int BOARD_COLUMNS = 9;
+	
 	public static void main(String[] args) {
 		// strona https://stackoverflow.com/questions/5831388/what-is-the-controller-in-java-swing
-
+			
 	       	SudokuModel      model      = new SudokuModel();
 	        SudokuView       view       = new SudokuView(model);
 	        SudokuController controller = new SudokuController(model, view);
--- SudokuController.java
+++ SudokuController.java
@@ -15,6 +15,7 @@ public class SudokuController {
         //... Add listeners to the view.
         view.addNewGameListener(new NewGameListener());
         view.addVerifyListener(new VerifyListener());
+        //view.addVerifyRangeListener(new VerifyRangeListener());
     }
     
     class NewGameListener implements ActionListener{
@@ -26,10 +27,74 @@ public class SudokuController {
     
     class VerifyListener implements ActionListener{
     	public void actionPerformed(ActionEvent e) {
+    		String[][] inputValues = m_view.getBoardValues();
+    		Integer[][] convertedValues = new Integer[9][9];
+    		boolean[][] invalidFields;
+    		boolean outOfRange = false;
     		
+    		
+    		m_view.eraseInvalidFields();
+    		
+    		for (int row = 0; row < 9; row++) {
+    			  for (int col = 0; col < 9; col++) {
+    				  
+    				  if (SudokuModel.isNumeric(inputValues[row][col]))
+    				  {
+    					  convertedValues[row][col] = Integer.parseInt(inputValues[row][col]);
+    					  if (!SudokuModel.isNumberInRange(convertedValues[row][col])) {
+        					  outOfRange = true;
+        					  m_view.markInvalidField(row, col);
+        				  }
+
+    				  }
+    				  else {
+    					  outOfRange = true;
+    					  m_view.markInvalidField(row, col);
+    					  
+    				  }
+			  
+    			  }
+    		}
+    		
+    		if (outOfRange)
+    			m_view.showMessage("Na czerwono zaznaczono wartoœci spoza zakresu 1-9.");
+    		else{
+    			if (m_model.isValidSudoku(convertedValues))
+    				m_view.showMessage("Gratulacje!!! Rozwi¹za³eœ Sudoku.");
+    			else
+    			{
+    			
+	    			invalidFields = m_model.getInvalidFields(convertedValues);
+	    				for (int row = 0; row < 9; row++) {
+	    					for (int col = 0; col < 9; col++) {
+	    						if(invalidFields[row][col] == true) {
+	    							m_view.markInvalidField(row, col);
+			       				  }
+	    					}
+	    				}
+    				m_view.showMessage("W zaznaczonych obszarach Sudoku wykryto zdublowane wartosci.");
+    			};
+
+    		}
     	}
-    }
     
-    
+	/*
+	 * class VerifyRangeListener implements ActionListener{ public void
+	 * actionPerformed(ActionEvent e) { String[][] values; values =
+	 * m_view.getBoardValues(); Integer[][] convertedValues = new Integer[9][9];
+	 * 
+	 * try { for (int row = 0; row < 9; row++) {
+	 * 
+	 * for (int col = 0; col < 9; col++) { convertedValues[row][col] =
+	 * Integer.parseInt(values[row][col]); }
+	 * 
+	 * } } catch (NumberFormatException e1) {
+	 * m_view.showError("Nieprawid³owa wartoœæ");
+	 * 
+	 * }
+	 * 
+	 * } }
+	 */
   
+    }
 }
